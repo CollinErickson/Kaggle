8c1ffdb43c5efd32a70aac02c1806b962da82e6e
Collin Erickson <collinberickson@gmail.com>
2016-10-08 21:34:07 -0500

    Added SVM, submitted results, gets 97.7%

--- DigitRecognizer/digit1.R
+++ DigitRecognizer/digit1.R
@@ -1,3 +1,5 @@
+library(magrittr)
+
 # Load in data
 trainALL <- read.csv("DigitRecognizer/train.csv")
 testALL <- read.csv("DigitRecognizer/test.csv")
@@ -52,3 +54,28 @@ table(prednet2, test$label)
 sum(prednet2 == test$label) / length(prednet2) 
 # 83.2% with maxit=200 and size=20
 
+
+
+# SVM
+#use_cols <- which(sapply(train, function(xx){!all(xx==xx[1])}))
+# Only pick out the columns that aren't all zeros (had problem where only 1 was nonzero, so using 10%)
+use_cols <- which(sapply(train, function(xx){sum(xx==xx[1])/length(xx)<.9})) #.1 too big, label might not get included
+library(e1071)
+svm.model <- svm(factor(label) ~ ., data=train[, use_cols]) # takes a couple minutes
+svm.pred <- predict(svm.model, test)
+svm.pred %>% str
+table(svm.pred, test$label)
+sum(svm.pred == test$label) / nrow(test)
+# 94.6% very good
+# 97.9% after fixing .1 to .9
+
+#now run on all training data
+rm(train);rm(test);rm(use_cols);rm(svm.model);rm(svm.pred) # to make sure I don't use these
+use_cols <- which(sapply(trainALL, function(xx){sum(xx==xx[1])/length(xx)<.9}))
+svm.model <- svm(factor(label) ~ ., data=trainALL[, use_cols])
+svm.pred <- predict(svm.model, testALL)
+svm.pred %>% str
+table(svm.pred)#, testALL$label)
+outDF <- data.frame(ImageId=1:length(svm.pred), Label=svm.pred)
+write.csv(x=outDF, file="DigitRecognizer/svmSUBMISSION.csv", row.names = F)
+head(read.csv("DigitRecognizer/svmSUBMISSION.csv"))
