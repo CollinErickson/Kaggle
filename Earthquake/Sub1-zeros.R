258d176041b378d003e1cb38d31e7ef89e8601a6
Collin Erickson <collinberickson@gmail.com>
2019-02-13 08:35:31 -0600

    Submission with mean is a little better

--- Earthquake/Sub1-zeros.R
+++ Earthquake/Sub1-zeros.R
@@ -14,3 +14,42 @@ for (testfile in testfiles) {
   testfile_nocsv <- strsplit(testfile, ".csv")[[1]]
   cat(file=subfilepath, testfile_nocsv, ",0\n", append = TRUE, sep = "")
 }
+
+
+# Predict using the mean
+# Since csv is so big, get sample of rows
+# There are about 633 million rows, so need to use a sample method
+# lf <- LaF::laf_open_csv(LaF::detect_dm_csv(filename=paste0(folderpath,"train.csv"), sep=",", header=TE, factor_fraction = -1))
+# sampdf <- LaF::read_lines(lf, sample(1:nrow(lf), 100))
+# sampdf <- DMwR2::sampleCSV(file=paste0(folderpath,"train.csv"), nrLines = 100, header = TRUE)
+
+library(LaF)
+
+sample1 <- function(file, n) {
+  lf <- laf_open(detect_dm_csv(file, sep = ",", header = TRUE, factor_fraction = -1))
+  return(read_lines(lf, sample(1:nrow(lf), n)))
+}
+
+sampdf <- sample1(paste0(folderpath,"train.csv"), 1e4)
+sampdf %>% str
+sampdf %>% summary
+
+mean_value <- mean(sampdf$time_to_failure)
+mean_value # should be about 5.4541
+mean_value <- 5.4541
+
+subfilepath <- paste0(folderpath, "Sub1-mean.csv")
+cat(file = subfilepath, "seg_id,time_to_failure\n")
+for (testfile in testfiles) {
+  testfile_nocsv <- strsplit(testfile, ".csv")[[1]]
+  cat(file=subfilepath, testfile_nocsv, ",", mean_value,"\n", append = TRUE, sep = "")
+}
+
+
+
+# Try to read and skip a lot or rows, is it slow?
+system.time(tmpdf <- read.csv(paste0(folderpath,"train.csv"), skip = 10000, nrows = 100, header=FALSE))
+system.time(tmpdf <- read.csv(paste0(folderpath,"train.csv"), skip = 100000, nrows = 100, header=FALSE))
+system.time(tmpdf <- read.csv(paste0(folderpath,"train.csv"), skip = 1000000, nrows = 100, header=FALSE))
+system.time(tmpdf <- read.csv(paste0(folderpath,"train.csv"), skip = 10000000, nrows = 100, header=FALSE))
+system.time(tmpdf <- readr::read_csv(paste0(folderpath,"train.csv"), skip = 10000000, n_max = 100))
