d54cfdac06154a0f7d6e0daaf55f791c478b35f3
Collin Erickson <collinberickson@gmail.com>
2019-02-15 21:26:09 -0600

    Made data from groups of 15k, svm gets me to 18 %ile

---
+++ Earthquake/FindGroups2.R
@@ -0,0 +1,146 @@
+folderpath <- "./Earthquake/"
+
+nlines <- 629145481
+filemap <- apply(expand.grid(letters, letters[1:3])[,c(2,1)], 1, function(x) paste0(x,collapse=''))[1:63]
+
+
+# numperfile <- 1000*100
+# newcsvpath <- paste0(folderpath,"trainsmall2.csv")
+# if (file.exists(newcsvpath)) {stop("file exists 87987")}
+# eps <- .0000001
+
+N <- 15000
+savegroupinfofilepath <- paste0(folderpath,"GroupInfo2.csv")
+for (i.file in 1:63) {
+  # print(i)
+  # skipi <- ceiling(runif(1)*(nlines-10))
+  # tmpdf <- data.table::fread(paste0(folderpath,"train.csv"), nrows = chunksize, skip = skipi, header = F)
+  traini <- data.table::fread(paste0(folderpath,"train/trainsplit", filemap[i.file],".csv"), header = (i.file==1))
+  colnames(traini) <- c("acoustic_data", "time_to_failure")
+  # indsi <- sample(1:nrow(tmpdfall), numperfile, replace=F)
+  # tmpdf <- tmpdfall[indsi,]
+  # print(str(tmpdf))
+  # write.table(x=tmpdf,file=newcsvpath,append=TRUE, row.names = FALSE, col.names=FALSE, sep=",")
+  # read.csv(newcsvpath,header = F)
+  startrow <- 1#4096 # 1
+  endrow <- startrow+N-1
+  while(endrow < nrow(traini)) {
+    print(c(i.file, startrow))
+    traini_chunk <- traini[startrow:endrow]
+    # Make sure all from same group
+    if (any(abs(diff(traini_chunk$time_to_failure)) > 1)) {#browser()
+      print("going to next since chunk moves too much, summary is")
+      print(summary)
+      # Increment for next section
+      startrow <- endrow+1
+      endrow <- startrow + N-1
+      next()
+    }
+    # All good so calculate stats
+    mean_acoustic <- mean(traini_chunk$acoustic_data)
+    sd_acoustic <- sd(  traini_chunk$acoustic_data)
+    absz <- abs(traini_chunk$acoustic_data - mean_acoustic) / sd_acoustic
+    chunkstats <- c(
+      i.file, startrow, endrow,
+      mean(traini_chunk$time_to_failure),
+      min( traini_chunk$time_to_failure),
+      max( traini_chunk$time_to_failure),
+      mean_acoustic, #mean(traini_chunk$acoustic_data),
+      min( traini_chunk$acoustic_data),
+      max( traini_chunk$acoustic_data),
+      sd_acoustic, #sd(  traini_chunk$acoustic_data)
+      quantile(absz, c(.25,.5,.75,.9,.95,.99,.999, 1)),
+      sum(absz>1)/N,sum(absz>3)/N,sum(absz>10)/N
+    )
+    # Write to file
+    cat(chunkstats,
+      file=savegroupinfofilepath, sep=',', append=T)
+    cat("\n", file=savegroupinfofilepath, append=T)
+    
+    # Increment for next section, NEED this before next above too!
+    startrow <- endrow+1
+    endrow <- startrow + N-1
+  }
+  # n <- 1
+  # while(traini$time[i] >= tmin-eps && traini$time[i] <= tmax+eps) {
+  #   tmin <- min(tmin, traini$time[i])
+  #   tmax <- max(tmax, traini$time[i])
+  #   i <- i+1
+  #   n <- n+1
+  # }
+  # stop()
+}
+
+# 
+# # for aa, 1:4095 is first group
+# plot(traini$acoustic_data[1:4095])
+# plot(traini$time_to_failure[1:4095])
+# plot(traini$acoustic_data[1:4095+4095])
+# plot(traini$time_to_failure[1:4095+4095])
+# plot(traini$acoustic_data[1:4095+4095*2+1])
+# plot(traini$time_to_failure[1:4095+4095*2+1])
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# # Find all groups
+# eps <- .0000001
+# i.file <- 1
+# i <- 1
+# traini <- NULL
+# groupnum <- 1
+# savegroupinfofilepath <- paste0(folderpath,"GroupInfo.csv")
+# 
+# while(TRUE) { # Loop over groups
+#   print(i)
+#   # Load file if needed
+#   if (is.null(traini)) {
+#     traini <- data.table::fread(paste0(folderpath,"train/trainsplit", filemap[i.file],".csv"), header = (i.file==1))
+#     colnames(traini) <- c("acoustic_data", "time_to_failure")
+#   }
+#   startrow <- i#4096 # 1
+#   tmin <- traini$time[startrow]
+#   tmax <- traini$time[startrow]
+#   i <- startrow + 1
+#   n <- 1
+#   # This will split up groups between files
+#   while(i <= nrow(traini) && traini$time[i] >= tmin-eps && traini$time[i] <= tmax+eps) {
+#     tmin <- min(tmin, traini$time[i])
+#     tmax <- max(tmax, traini$time[i])
+#     i <- i+1
+#     n <- n+1
+#   }
+#   endrow <- i-1
+#   if (i > nrow(traini)) {
+#     traini <- NULL
+#     i.file <- i.file + 1
+#     i <- 1
+#   }
+#   
+#   if (F) {# slower
+#     plot(traini$acoustic_data[startrow:(i-1)], main=round(mean(traini$time_to_failure[startrow:(i-1)]),4))
+#   }
+#   cat("finished ", i.file, i, n, '\n')
+#   # Next group
+#   groupnum <- groupnum + 1
+#   if (endrow > startrow) {
+#     cat(groupnum, i.file, startrow, endrow,
+#         mean(traini$time_to_failure[startrow:endrow]),
+#         min( traini$time_to_failure[startrow:endrow]),
+#         max( traini$time_to_failure[startrow:endrow]),
+#         mean(traini$acoustic_data[startrow:endrow]),
+#         min( traini$acoustic_data[startrow:endrow]),
+#         max( traini$acoustic_data[startrow:endrow]),
+#         sd(  traini$acoustic_data[startrow:endrow]),
+#         file=savegroupinfofilepath, sep=',', append=T)
+#     cat("\n", file=savegroupinfofilepath, append=T)
+#   } else { # single point in group
+#     cat(paste("single point in group", groupnum, startrow, endrow, i.file))
+#   }
+#   # stop()
+# }
+# 
+# # Looks like groups come in sizes of 4096
---
+++ Earthquake/Sub5-15kgroupwithfeaturesandsvm.R
@@ -0,0 +1,155 @@
+folderpath <- "./Earthquake/"
+
+filemap <- apply(expand.grid(letters, letters[1:3])[,c(2,1)], 1, function(x) paste0(x,collapse=''))[1:63]
+groupinfofilepath <- paste0(folderpath,"GroupInfo2.csv")
+gdf <- read.csv(groupinfofilepath, header = F)
+gdf %>% head
+colnames(gdf) <- c("i.file", "startrow", "endrow",
+                   "meantime","mintime","maxtime",
+                   "meanacoustic","minacoustic","maxacoustic","sdacoustic",
+                   "q25", "q50","q75", "q90", "q95","q99","q999","q1",
+                   "p1","p3","p10")
+# gdf$nrow <- gdf$endrow - gdf$startrow
+gdf %>% head
+gdf %>% str()
+gdf %>% summary
+plot(gdf$meanacoustic, gdf$meantime)
+plot(gdf$sdacoustic, gdf$meantime)
+summary(lm(meantime ~ meanacoustic, gdf))$r.sq
+summary(lm(meantime ~ meanacoustic + sdacoustic, gdf))$r.sq
+summary(lm(meantime ~ sdacoustic, gdf))$r.sq
+
+gdf$minacoustic %>% is.infinite() %>% table
+gdf$maxacoustic %>% is.infinite() %>% table
+gdf[(gdf$maxacoustic %>% is.infinite()),]
+
+# Remove bad rows, remove unneeded columns
+gdf2 <- gdf[!(gdf$maxacoustic %>% is.infinite()), -c(1,2,3,5,6)]
+summary(lm(meantime ~ meanacoustic + sdacoustic, gdf2))$r.sq
+gdf2$rangeacoustic <- gdf2$maxacoustic - gdf2$minacoustic
+plot(gdf2$rangeacoustic, gdf2$meantime)
+plot(gdf2$sdacoustic / gdf2$rangeacoustic, gdf2$meantime)
+summary(lm(meantime ~ meanacoustic + sdacoustic + rangeacoustic, gdf2))$r.sq
+
+
+lm3 <- lm(meantime ~ meanacoustic + sdacoustic + rangeacoustic, gdf2)
+SGGP::valstats(predict(lm3, gdf2), rep(0,nrow(gdf2)), gdf2$meantime)
+
+
+tr.inds <- sample(1:nrow(gdf2), floor(.7*nrow(gdf2)))
+tr1 <- gdf2[tr.inds,]
+te1 <- gdf2[-tr.inds,]
+
+lm0 <- lm(meantime ~ 1, tr1)
+summary(lm0)$r.sq
+pr0tr <- predict(lm0, tr1, se=T)
+SGGP::valstats(pr0tr$fit, pr0tr$se.fit, tr1$meantime)
+pr0te <- predict(lm0, te1, se=T)
+SGGP::valstats(pr0te$fit, pr0te$se.fit, te1$meantime)
+
+lm1 <- lm(meantime ~ meanacoustic + sdacoustic + rangeacoustic, tr1)
+summary(lm1)$r.sq
+pr1tr <- predict(lm1, tr1, se=T)
+SGGP::valstats(pr1tr$fit, pr1tr$se.fit, tr1$meantime)
+pr1te <- predict(lm1, te1, se=T)
+SGGP::valstats(pr1te$fit, pr1te$se.fit, te1$meantime)
+
+lm2 <- lm(meantime ~ ., tr1)
+summary(lm2)$r.sq
+pr2tr <- predict(lm2, tr1, se=T)
+SGGP::valstats(pr2tr$fit, pr2tr$se.fit, tr1$meantime)
+pr2te <- predict(lm2, te1, se=T)
+SGGP::valstats(pr2te$fit, pr2te$se.fit, te1$meantime)
+
+
+svm1 <- e1071::svm(meantime ~ meanacoustic + sdacoustic + rangeacoustic, data=tr1)
+pr.svm1tr <- predict(svm1, tr1, se=T)
+SGGP::valstats(pr.svm1tr, rep(1,nrow(tr1)), tr1$meantime)
+pr.svm1te <- predict(svm1, te1, se=T)
+SGGP::valstats(pr.svm1te, rep(1,nrow(te1)), te1$meantime)
+plot(te1$meantime, pr.svm1te)
+#  RMSE    score CRPscore  coverage      corr        R2
+#1 3.260681 10.63204 2.120923 0.4707146 0.4659226 0.2089533
+
+
+svm0 <- e1071::svm(meantime ~ ., data=tr1)
+pr.svm0tr <- predict(svm0, tr1, se=T)
+SGGP::valstats(pr.svm0tr, rep(1,nrow(tr1)), tr1$meantime)
+pr.svm0te <- predict(svm0, te1, se=T)
+SGGP::valstats(pr.svm0te, rep(1,nrow(te1)), te1$meantime)
+plot(te1$meantime, pr.svm0te);abline(a=0,b=1,col=2)
+# RMSE    score CRPscore  coverage      corr        R2
+# 3.066802 9.405273 1.959223 0.5071622 0.5466267 0.2898688
+
+
+svm2 <- e1071::svm(meantime ~ meanacoustic + sdacoustic + rangeacoustic + I(sdacoustic/rangeacoustic), data=tr1)
+pr.svm2tr <- predict(svm2, tr1, se=T)
+SGGP::valstats(pr.svm2tr, rep(1,nrow(tr1)), tr1$meantime)
+pr.svm2te <- predict(svm2, te1, se=T)
+SGGP::valstats(pr.svm2te, rep(1,nrow(te1)), te1$meantime)
+plot(te1$meantime, pr.svm2te);abline(a=0,b=1,col=2)
+
+
+svm3 <- e1071::svm(meantime ~ meanacoustic + sdacoustic + rangeacoustic + I(sdacoustic/rangeacoustic) + I(meanacoustic/sdacoustic), data=tr1)
+pr.svm3tr <- predict(svm3, tr1, se=T)
+SGGP::valstats(pr.svm3tr, rep(1,nrow(tr1)), tr1$meantime)
+pr.svm3te <- predict(svm3, te1, se=T)
+SGGP::valstats(pr.svm3te, rep(1,nrow(te1)), te1$meantime)
+plot(te1$meantime, pr.svm3te);abline(a=0,b=1,col=2)
+
+getDmatrix1 <- function(x) {
+  # xgboost::xgb.DMatrix(cbind(x$meanacoustic, x$sdacoustic, x$rangeacoustic, x$sdacoustic/x$rangeacoustic + x$meanacoustic/x$sdacoustic), label=x$meantime)
+  xgboost::xgb.DMatrix(as.matrix(x[,-1]), label=x$meantime)
+}
+xgb1 <- xgboost::xgb.train(data=getDmatrix1(tr1), nrounds = 3000)
+pr.xgb1tr <- predict(xgb1, getDmatrix1(tr1), se=T)
+SGGP::valstats(pr.xgb1tr, rep(1,nrow(tr1)), tr1$meantime)
+pr.xgb1te <- predict(xgb1, getDmatrix1(te1), se=T)
+SGGP::valstats(pr.xgb1te, rep(1,nrow(te1)), te1$meantime)
+plot(te1$meantime, pr.xgb1te);abline(a=0,b=1,col=2)
+
+
+# Make predictions for submission
+# Sub5-svm was made with svm0, all features from file
+if (F) {
+  testfolderpath <- paste0(folderpath, "test/")
+  testsubmissionpath <- paste0(folderpath,"Sub5-svm.csv")
+  if (file.exists(testsubmissionpath)) {stop("File already exists")}
+  cat(file=testsubmissionpath, "seg_id,time_to_failure\n")
+  i <- 0
+  for (testfile in list.files(testfolderpath)) {
+    i <- i+1; cat(i, testfile, "\n")
+    testdf <- read.csv(paste0(testfolderpath, testfile))
+    names(testdf) <- "V1"
+    
+    test.absz <- abs(testdf$V1 - mean(testdf$V1)) / sd(testdf$V1)
+    # N <- nrow(testdf)
+    testpred <- predict(svm0, data.frame(meanacoustic=mean(testdf$V1),
+                                         # rangeacoustic=max(testdf$V1)-min(testdf$V1),
+                                         minacoustic=min(testdf$V1),
+                                         maxacoustic=max(testdf$V1),
+                                         sdacoustic=sd(testdf$V1),
+                                         q25=quantile(test.absz, .25),
+                                         q50=quantile(test.absz, .5),
+                                         q75=quantile(test.absz, .75),
+                                         q90=quantile(test.absz, .90),
+                                         q95=quantile(test.absz, .95),
+                                         q99=quantile(test.absz, .99),
+                                         q999=quantile(test.absz, .999),
+                                         q1=quantile(test.absz, 1),
+                                         p1=sum(test.absz>1)/nrow(testdf),
+                                         p3=sum(test.absz>3)/nrow(testdf),
+                                         p10=sum(test.absz>10)/nrow(testdf)
+                                         ))
+    # testpred <- predict(svm3, getDmatrix1(data.frame(meanacoustic=mean(testdf$V1),
+    #                                                  rangeacoustic=max(testdf$V1)-min(testdf$V1),
+    #                                                  sdacoustic=sd(testdf$V1),
+    #                                                  meantime=NaN)))
+    # Some preds are negative, make them zero. Highest is 16.1
+    testpred <- pmax( 0.05, testpred)
+    testpred <- pmin(16, testpred)
+    testfile_nocsv <- strsplit(testfile, ".csv")[[1]]
+    cat(paste0(testfile_nocsv, ",", (testpred), "\n"), file=testsubmissionpath, sep = "",append=TRUE)
+    # stop()
+  }
+}
